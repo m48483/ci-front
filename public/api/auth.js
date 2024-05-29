defff621ebc1fe12ea4078a948235f03a0d66a3b
m48483 <71882579+m48483@users.noreply.github.com>
2024-05-29 16:24:48 +0900

    dsfsf

--- public/api/auth.js
+++ public/api/auth.js
@@ -1,21 +1,52 @@
 import { api } from "../config/network";
 
 export const saveBoard = async (data) => {
-  const res = await api("/api/boards", "post", data);
+  const res = await axios({
+    url: "/api/boards",
+    method: "post",
+    data: data,
+    headers: {
+      Authorization: localStorage.getItem("id"),
+    },
+  });
+
   return res;
 };
 
 export const getBoard = async () => {
-  const res = await api("/api/boards", "get");
+  const res = await axios({
+    url: "/api/boards",
+    method: "get",
+    data: data,
+    headers: {
+      Authorization: localStorage.getItem("id"),
+    },
+  });
+
   return res;
 };
 
 export const delBoard = async () => {
-  const res = await api("/api/boards", "delete");
+  const res = await axios({
+    url: "/api/boards",
+    method: "delete",
+    data: data,
+    headers: {
+      Authorization: localStorage.getItem("id"),
+    },
+  });
+
   return res;
 };
 
-export const delByBoard = async (data) => {
-  const res = await api("/api/boards/" + data, "delete");
+export const delByBoard = async (boardId) => {
+  const res = await axios({
+    url: `/api/boards/${boardId}`,
+    method: "delete",
+    headers: {
+      Authorization: localStorage.getItem("id"),
+    },
+  });
+
   return res;
 };
